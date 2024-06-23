0dbf4e090308dc49227ba5e023d54d5575cb7283
ansarkp10 <160372516+ansarkp10@users.noreply.github.com>
2024-06-23 20:33:53 +0530

    Update settings.py

--- akmedia/settings.py
+++ akmedia/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'akmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres'
+        'PASSWORD': 'MGfIzTFMNaOpNROzIOdHgujAaKQpOkPb'
+        'HOST': 'roundhouse.proxy.rlwy.net'
+        'PORT': '37579'
     }
 }
 DATABASES['default'] = dj_database_url.config()
