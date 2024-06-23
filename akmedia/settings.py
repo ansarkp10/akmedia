3f6793b408479eef00c7a62ecbf20bde528f085f
ansarkp10 <160372516+ansarkp10@users.noreply.github.com>
2024-06-23 17:30:41 +0530

    Update settings.py

--- akmedia/settings.py
+++ akmedia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+*jh7iq+z0!y3y@6s*-g%&&0s255a1)^33%m)vk-g2vj&clx0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
