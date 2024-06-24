325ba99155478200dc743fb8c9f31e89b76e6d68
ansarkp10 <160372516+ansarkp10@users.noreply.github.com>
2024-06-24 11:09:35 +0530

    Update urls.py

--- akmedia/urls.py
+++ akmedia/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from  django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +9,5 @@ urlpatterns = [
     path('', include('content.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
